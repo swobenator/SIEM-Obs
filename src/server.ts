81f4c187df37934fea591ba63fdb2038682a2bdd
swobenator <soheibtgf@gmail.com>
2026-08-15 17:21:16 +0100

    Add PostgreSQL dev environment

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,46 @@
 import express from "express";
+import { pool } from "./database.js"
 
 const app = express();
 const PORT = 3000;
 
+interface LogEvent {
+    id: string,
+    timestamp: Date,
+    level: "INFO" | "ERROR" | "ALERT",
+    message: string;
+}
+
+let event: LogEvent = { 
+    id: "123",
+    timestamp: new Date(),
+    level: "ERROR",
+    message: "Database connection failed"
+ };
+
+
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-    res.json({status: "ok"});
+    res.json({ status: event.message });
+});
+
+app.get("/api/health", async (_req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()")
+
+        res.json({
+            status: "ok",
+            databaseTime: result.rows[0].now,
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+             status: "error",
+             message: "Database connection failed", 
+            });
+    }
 });
 
 app.listen(PORT, () => {
